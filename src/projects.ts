bfdd7f0c87d43f008f6323f53bbf7021039a048c
Lambent7 <graham2md@gmail.com>
2026-03-31 19:24:19 -0500

    switched video and image formats for better compression (av1/avif)

--- src/projects.ts
+++ src/projects.ts
@@ -13,26 +13,50 @@ export interface Project {
 
 export const myProjects: Project[] = [
     {
-        id: "emotion_game",
-        title: "Emotion Game",
-        description: "A speedrunning game to type sentences that have a sense of the target emotion. An emotion classifying AI decides if you win or lose!",
-        imagePath: "assets/emotion_game.jpg",
-        videoPath: "assets/emotion_game.mp4",
-        url: "https://github.com/Lambent7/Emotion-Game",
-        hasGithubRepo: true,
-        isFavorite: false,
-        datePosted: new Date("2026-03-31"),
-        filters: ["TypeScript", "API", ""]
-    },
-    {
         id: "black_crown",
         title: "Black Crown Game Console",
-        description: "Custom gaming console powered by the BC-250 (PS5) APU Board.",
-        imagePath: "assets/black_crown.jpg",
+        description: "Custom gaming console powered by the BC-250 (PS5) APU Board and CachyOS (Arch Linux Fork).",
+        imagePath: "assets/black_crown.avif",
         url: "blog/black_crown.html",
         hasGithubRepo: false,
         isFavorite: true,
         datePosted: new Date("2026-03-31"),
-        filters: ["Hardware", "Linux", "3D Modeling", ]
-    }
+        filters: ["Hardware", "Linux", "3D Modeling", "Quickshell", "UI/UX"]
+    },
+    {
+        id: "emotion_game",
+        title: "Emotion Game",
+        description: "A speedrunning game to type sentences that have a sense of the target emotion. An emotion classifying AI decides if you win or lose!",
+        imagePath: "assets/emotion_game.avif",
+        videoPath: "assets/emotion_game.av1",
+        url: "https://github.com/Lambent7/Emotion-Game",
+        hasGithubRepo: true,
+        isFavorite: false,
+        datePosted: new Date("2026-03-31"),
+        filters: ["NLP", "AI", "UI/UX", "Python", "Qt"]
+    },
+    {
+        id: "steam_review_classifier",
+        title: "Steam Review Classifier",
+        description: "",
+        imagePath: "assets/steam_review_classifier.avif",
+        videoPath: "assets/steam_review_classifier.av1",
+        url: "https://github.com/Lambent7/SteamReviewClassifier",
+        hasGithubRepo: true,
+        isFavorite: false,
+        datePosted: new Date("2026-03-31"),
+        filters: [""]
+    },
+    {
+        id: "",
+        title: "",
+        description: "",
+        imagePath: "assets/.avif",
+        videoPath: "assets/.av1",
+        url: "https://github.com/Lambent7/",
+        hasGithubRepo: true,
+        isFavorite: false,
+        datePosted: new Date("2026-03-31"),
+        filters: [""]
+    },
 ];
